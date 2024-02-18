c132066ca5f1ee6e402b8fc62529ce149cf1183b
Sashok <zh.alexander.qq@gmail.com>
2024-02-18 04:02:50 +0300

    try exeption

--- src/main/java/com/example/configuration/ApplicationConfig.java
+++ src/main/java/com/example/configuration/ApplicationConfig.java
@@ -6,13 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
+
+import java.util.List;
+import java.util.Properties;
 
 
 @EnableWebMvc
@@ -44,6 +49,25 @@ public class ApplicationConfig implements WebMvcConfigurer, ApplicationContextAw
         return engine;
     }
 
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(exceptionResolver());
+    }
+
+    @Bean
+    public SimpleMappingExceptionResolver exceptionResolver() {
+        SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
+
+        // Настройка сопоставлений исключений с URL-адресами
+        Properties exceptionMappings = new Properties();
+        exceptionMappings.put("org.springframework.web.servlet.NoHandlerFoundException", "error");
+
+        exceptionResolver.setExceptionMappings(exceptionMappings);
+        exceptionResolver.setDefaultErrorView("error-page"); // Обработка ошибки по умолчанию
+
+        return exceptionResolver;
+    }
+
     private SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
         resolver.setApplicationContext(applicationContext);
@@ -53,6 +77,6 @@ public class ApplicationConfig implements WebMvcConfigurer, ApplicationContextAw
         return resolver;
     }
 
-
-
 }
+
+
--- src/main/java/com/example/repository/implementation/EmployeeRepositoryImp.java
+++ src/main/java/com/example/repository/implementation/EmployeeRepositoryImp.java
@@ -27,7 +27,7 @@ public class EmployeeRepositoryImp implements EmployeeRepository {
         return (Employee) employees.stream()
                 .filter(employee -> employee.getId() == id)
                 .findFirst()
-                .orElse(null);
+                .orElseThrow(() -> new IllegalArgumentException("Employee with id " + id + " not found"));
     }
 
     @Override
@@ -60,7 +60,11 @@ public class EmployeeRepositoryImp implements EmployeeRepository {
 
     @Override
     public void deleteEmployee(int id) {
-        employees.removeIf(employee -> employee.getId() == id);
+        boolean remowed = employees.removeIf(employee -> employee.getId() == id);
+        if (!remowed) {
+            throw new IllegalArgumentException("Employee with id " + id + " not found");
+        }
+
     }
 
     @Override
